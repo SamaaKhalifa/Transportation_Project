97d914b1f343b18a15649fed09a62738f0e85742
NouraAshraff <62406338+NouraAshraff@users.noreply.github.com>
2021-11-22 00:08:41 +0200

    semi-completed functions

--- src/com/company/Area.java
+++ src/com/company/Area.java
@@ -5,21 +5,31 @@ import java.util.ArrayList;
 
 public class Area implements IArea {
 
-    private ArrayList<IDriver> drivers=new ArrayList<>();
+    private ArrayList<IDriver> drivers = new ArrayList<>();
     private String name;
 
+    public void notifyDriver(Ride ride) {
+        for (int i = 0; i < drivers.length(); i++) {
+            drivers.indexOf(i).update();
 
-public Offer notifyDriver (Ride ride){
-    for(int i=0;i<drivers.length() ;i++){
-        drivers.indexOf(i).update();
+        }
+    }
+
+    public void addDriver(Driver driver) {
+        drivers.add(driver);
 
     }
-}
-public void addDriver ( Driver driver){
 
-}
-public void removeDriver ( Driver driver){
+    public void removeDriver(Driver driver) {
+        drivers.remove(driver);
+        //drivers.remove(new Driver (driver));
 
-}
+    }
+
+    @Override
+    public String toString() {
+        return "Area [drivers=" + drivers + ", name=" + name + "]";
+    }
     
+
 }
